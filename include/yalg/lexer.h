2c109d7a14442f1e1c143d969a3fd4de8e0fb704
Fionn Langhans <fionn.langhans@gmail.com>
2019-05-20 07:02:59 +0200

    more Token func.

--- include/yalg/lexer.h
+++ include/yalg/lexer.h
@@ -7,8 +7,7 @@
 
 #include "yalg/global.hpp"
 
-namespace yalg {
-  typedef uint32_t TokenType;
+typedef uint32_t TokenType;
 
 #define  TOK_ID      256,     //!< identifier
 #define  TOK_STR     257,     //!< string (C++)
@@ -17,17 +16,47 @@ namespace yalg {
   
 #define  TOK_ERR     300,     //!< Token value reserved for errors
 
-  struct Token {
-	  TokenType type;
-	  union value {
-		  char ch;
-		  char * str;
-	  };
+typedef struct {
+  TokenType type; //!< Type of the token
+  union value {
+	  char ch;      //!< Character value TOK_CHAR
+	  char * str;   //!< String value TOK_ID, TOK_STR
   };
+} Token;
 
-  /*!\brief Frees memory allocated by tok
-   */
-  void Token_free(Token *tok);
-}
+/*!\brief Allocate token.
+ * \param type Type of the Token to allocate
+ * \return Returns NULL if failed to dynamically allocate storage, otherwise
+ * not NULL (reference to memory segment).
+ */
+Token *Token_alloc(TokenType type);
+
+/*!\brief Frees memory allocated by tok
+ * \param tok Memory to free
+ */
+void Token_free(Token *tok);
+
+/*!\param tok Token to copy
+ * \return Returns copied token tok. If dynamic allocation failed NULL.
+ */
+Token *Token_copy(Token *tok);
+
+typedef struct {
+  FILE *input;    //!< Input stream
+
+  char ch;        //!< Current character
+  Token *tok;     //!< Current token
+
+  size_t line;    //!< Current line number
+} Lexer;
+
+/*!\brief Initializes lexer. Read-operation on input.
+ * \param lex
+ */
+void Lexer_init(Lexer *lex);
+
+/*!\brief Read next token.
+ */
+Token *Lexer_next(Lexer *lex);
 
 #endif /* YALG_LEXER_HPP */
--- src/lexer.c
+++ src/lexer.c
@@ -1,5 +1,16 @@
 #include "yalg/lexer.h"
 
+// Token
+
+Token *Token_alloc(TokenType type) {
+  Token *tok = (Token*) malloc(sizeof(Token));
+  if (!tok) return NULL;
+
+  tok->type = type;
+
+  return tok;
+}
+
 void Token_free(Token *tok) {
 	switch(tok->type) {
 		case TOK_ID:
@@ -7,7 +18,46 @@ void Token_free(Token *tok) {
 			free(tok->value.str);
 			break;
 		default:
-	};
+	}
 
+  free(tok);
+}
 
+Token *Token_copy(Token *tok) {
+  Token *result = Token_alloc(tok->type);
+  if (!result) return NULL;
+
+  switch (tok->type) {
+    case TOK_ID:
+    case TOK_STR:
+      result->value.str = strcpy(tok->value.str);
+      if (!result->value.str) {
+        free(result);
+        return NULL;
+      }
+
+      break;
+    case TOK_CH:
+      result->value.ch = strcpy(tok->value.ch);
+      break;
+    default:
+  }
+
+  return result;
+}
+
+// Lexer
+
+Token *Lexer_next(Lexer *lex) {
+  switch (lex->value.ch) {
+    case '[':
+    case '"':
+    case '\'':
+    case '{':
+    default:
+  }
+
+  if (isalpha(lex)) {
+
+  }
 }
